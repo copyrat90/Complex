de013665489334571387a4e7e78d9734bd7700c2
copyrat90 <copyrat90@gmail.com>
2018-10-04 19:10:09 +0900

    main() 함수 작업 완료

--- main.cpp
+++ main.cpp
@@ -1,64 +1,63 @@
 #include "common.h"
 #include "complex.h"
+#include "rational.h"
 
 using namespace std;
 
 
-// 다시 고치는 중 
+void getNumDen(int& num, int& den)
+{
+	do
+	{
+		cout << "분자를 입력하세요 : ";
+		cin >> num;
+	}
+	while (!cin);
+	
+	do
+	{
+		cout << "분모를 입력하세요 : ";
+		cin >> den;
+		if (den == 0)
+		{
+			cout << "분모는 0일 수 없습니다." << endl;
+			continue;
+		}
+	}
+	while (!cin);
+}
+
+
+Complex getComplex(void)
+{
+	int num, den;
+
+	cout << "실수부를 입력합니다." << endl;
+	getNumDen(num, den);
+	Rational realPart(num, den);
+
+	cout << "허수부를 입력합니다." << endl;
+	getNumDen(num, den);
+	Rational imaginaryPart(num, den);
+
+	return Complex(imaginaryPart, realPart);
+}
+
+
 int main(int argc, char** argv)
-{	
-	int numTemp, denTemp;
-	
-	cout << "분수 덧셈 뺄셈 프로그램입니다." << endl;
+{
+	cout << "복소수 덧셈 뺄셈 프로그램입니다." << endl;
 	cout << "항 두개만 지원합니다." << endl << endl;
-	
+
 	cout << "첫 번째 숫자 입력입니다." << endl;
-	do
-	{
-		cout << "분자를 입력하세요 : ";
-		cin >> numTemp;
-	} while (!cin);
-	do
-	{
-		cout << "분모를 입력하세요 : ";
-		cin >> denTemp;
-		if (denTemp == 0)
-		{
-			cout << "분모는 0일 수 없습니다." << endl;
-			continue;
-		}
-	} while (!cin);
-	
-#ifdef _DEBUG_MODE_
-	cout << "Rational r1(" << numTemp << ", " << denTemp << ")\n";
-#endif
-	Rational r1(numTemp, denTemp);
-	
+	Complex c1 = getComplex();
+
 	cout << "두 번째 숫자 입력입니다." << endl;
-	do
-	{
-		cout << "분자를 입력하세요 : ";
-		cin >> numTemp;
-	} while (!cin);
-	do
-	{
-		cout << "분모를 입력하세요 : ";
-		cin >> denTemp;
-		if (denTemp == 0)
-		{
-			cout << "분모는 0일 수 없습니다." << endl;
-			continue;
-		}
-	} while (!cin);
-	
-#ifdef _DEBUG_MODE_
-	cout << "Rational r2(" << numTemp << ", " << denTemp << ")\n";
-#endif
-	Rational r2(numTemp, denTemp);
-	
+	Complex c2 = getComplex();
+
 	cout << endl;
 	char fractionOperator;
-	Rational rResult;
+	Complex cResult;
 
 	while (true)
 	{
@@ -68,21 +67,21 @@ int main(int argc, char** argv)
 
 		if (fractionOperator == '+')
 		{
-			rResult = r1.add(r2);
+			cResult = c1 + c2;
 			break;
 		}
 		else if (fractionOperator == '*')
 		{
-			rResult = r1.multiply(r2);
+			cResult = c1 * c2;
 			break;
 		}
 		else
 			cout << "연산자가 잘못 입력되었습니다." << endl;
 	}
-	
+
 	cout << "== ";
-	rResult.print();
+	cResult.print();
 	cout << endl;
-	
+
 	return 0;
 }
